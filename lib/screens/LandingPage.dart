b1ed4a1a5f48861076061642087e3366399de4ae
Haider Irfan <haiderirfan62batth@gmail.com>
2023-11-20 22:34:58 +0500

    First commit

---
+++ lib/screens/LandingPage.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("lib/assets/LandingPage_background.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+        child: Column(
+          children: [
+            SizedBox(height: screenSize.height * 0.80),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => LandingPage(),
+                    ),
+                  );
+                },
+                child: Text('dekhte hain', style: TextStyle(fontSize: 24),),
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)
+                  ),
+                  backgroundColor: Color(0xFF357C93),
+                  padding: EdgeInsets.only(left: 50, top: 12, right: 50, bottom: 12),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
