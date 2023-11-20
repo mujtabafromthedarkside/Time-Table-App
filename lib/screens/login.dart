6ae76a9d6cf75cf279fc5c0f468ed93beb27c145
Muhammad Abdullah <126229662+Abdullah2022323@users.noreply.github.com>
2023-11-21 01:08:02 +0500

    first commit

---
+++ lib/screens/login.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import '../config/config.dart';
+
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("lib/assets/background1.png"),
+            fit: BoxFit.cover,
+          ),
+          color: backgroundColor,
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 50),
+              Image(image: AssetImage(
+                "lib/assets/gikilogo.png",
+              ),
+                height: 100,
+                width: 100,
+              ),
+              SizedBox(height: 50),
+              Text(
+                'Login',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 50),
+              Container(
+                width: 370,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: TextField(
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          hintText: 'Email',
+                          hintStyle: TextStyle(color: Colors.grey), // Set hint color to gray
+                          filled: true,
+                          fillColor: backgroundColor, // Set background color to white
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: BorderSide(color: Colors.white,width: 4),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.blue,width: 4),
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: TextField(
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          hintStyle: TextStyle(color: Colors.grey), // Set hint color to gray
+                          filled: true,
+                          fillColor: backgroundColor, // Set background color to white
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: BorderSide(color: Colors.white,width: 4),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.blue,width: 4),
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 50),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 80),
+                      child: SizedBox(
+                        width: double.infinity,
+                        child: Container(
+                          width: 150,
+                          child: ElevatedButton(
+                            onPressed: () {
+
+                              //to be yet implemented
+                            },
+                            child: Text('Sign in   ->',style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),),
+                            style: ElevatedButton.styleFrom(
+                              primary: backgroundColor,
+                              padding: EdgeInsets.symmetric(vertical: 15),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50),
+                                side: BorderSide(color: Colors.white,width: 4),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
+
+  }
+
+}
