3339c53dc2ee66b2240c94c5fe7675cbe541fb43
Mujtaba <72826134+mujtabafromthedarkside@users.noreply.github.com>
2024-02-02 19:38:02 +0500

    added color_variables, removed dropdown underlines
    background dark, text light

---
+++ lib/config/strings.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color LandingPageBrightYellow = Color(0xFFE9A21A);
+Color LandingPageLightBlue = Color(0xFF357C93);
+Color LandingPageDarkBlue = Color(0xFF0E2954);
